a3cb0f9fd19f8dc8caa633f3d2b3ce58a31d5c49
unknown <chukwuemika11>
2024-04-09 22:28:02 +0100

    nexr-practice

---
+++ src/app/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div>
+     <p>PageNotFound</p> 
+     <Link href="/">return home</Link>
+    </div>
+  )
+}
+
+export default PageNotFound
---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SinglePostPage = () => {
+  return (
+    <div>
+    <h1>SinglePostPage</h1>
+    </div>
+  )
+}
+
+export default SinglePostPage
